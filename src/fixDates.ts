5f9d486c9c5ced7c631c60b43a5a87b9a7c39c10
Tyler Menezes <x@tyler.vc>
2024-06-05 22:03:04 -0700

    Update due date time

--- src/fixDates.ts
+++ src/fixDates.ts
@@ -12,8 +12,8 @@ export async function fixDates() {
   for (const item of withoutTime) {
     console.log(`- Adding default due time to ${item.content}`);
     const dueDate = item.due.string
-      ? { string: `${item.due.string} at 5pm`, date: `${item.due.date}T17:00:00` }
-      : { date: `${item.due.date}T17:00:00` };
+      ? { string: `${item.due.string} at 4pm`, date: `${item.due.date}T16:00:00` }
+      : { date: `${item.due.date}T16:00:00` };
     await todoist.items.update({
       id: item.id,
       due: dueDate as unknown as DueDate, // API supports this but Todoist Node module doesn't.
