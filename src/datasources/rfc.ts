0724a7b20aa1478551e589947002c589db221c1a
Tyler Menezes <x@tyler.vc>
2024-06-06 18:13:31 -0700

    Add title

--- src/datasources/rfc.ts
+++ src/datasources/rfc.ts
@@ -2,9 +2,11 @@ import { obsidian } from '@/services';
 import { NoteEntry } from '@/services/Obsidian';
 import { markdownFrontmatter, markdownParse } from '@/utils';
 import { DateTime } from 'luxon';
+import { Heading, Text } from 'mdast';
 import schedule from 'node-schedule';
 
 interface NoteEnhance {
+  title: string
   tags: string[]
   image?: string
   slug: string
@@ -29,7 +31,10 @@ export async function rfcUpdate() {
         unlisted?: boolean,
         date?: string,
       }>(n.data);
+      const md = markdownParse(n.data);
+      const h1 = md.find(n => n.type === 'heading' && (n as Heading).depth === 1) as Heading;
       return {
+        title: h1.children.map(c => (c as Text).value).join(' '),
         path: n.path,
         slug: n.path
           .slice('rfc/'.length)
